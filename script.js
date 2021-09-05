78f53581e722b2250a5ccbb991c8c91a07788442
Luke <lukewebsterkhan@gmail.com>
2021-09-05 22:19:17 +0100

    added length validation

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
-const capital = document.getElementById('capital');
+const capital = document.getElementById("capital");
+const length = document.getElementById("length");
 
 function isUserNameValid(username) {
-    /* 
+  /* 
       Usernames can only have: 
       - Lowercase Letters (a-z) 
       - Numbers (0-9)
       - Dots (.)
       - Underscores (_)
     */
-    const res = /^[a-z0-9_\.]+$/.exec(username);
-    const valid = !!res;
-    return valid;
-  }
-
-// onclick for password field, dispaly message
-let myInput = document.getElementById('inputname')
-
-myInput.onfocus = function() {
-    document.getElementById('message').style.display = 'block';
+  const res = /^[a-z0-9_\.]+$/.exec(username);
+  const valid = !!res;
+  return valid;
 }
 
-myInput.onblur = function() {
-    document.getElementById('message').style.display = 'none';
-}
+// onclick for password field, display message
+let myInput = document.getElementById("inputname");
+
+myInput.onfocus = function () {
+  document.getElementById("message").style.display = "block";
+};
+
+myInput.onblur = function () {
+  document.getElementById("message").style.display = "none";
+};
 
 // When user types in field
 
-myInput.onkeyup = function() {
-// validate lowercase letter
-let lowerCaseLetters = /[a-z]/g;
-if(myInput.value.match(lowerCaseLetters)){
-letter.classList.remove('invalid');
-letter.classList.add('valid')
-} else {
-    letter.classList.remove('valid');
-    letter.classList.add('invalid')
-}
+myInput.onkeyup = function () {
+  // validate lowercase letter
+  let lowerCaseLetters = /[a-z]/g;
+  if (myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
 
-// validate capital letter
-let upperCaseLtters = /[A-Z]/g;
-if(myInput.value.match(upperCaseLtters)){
-  capital.classList.remove('invalid');
-  capital.classList.add('valid');
-} else {
-  capital.classList.remove('valid');
-  capital.classList.add('invalid');
-}
-}
+  // validate capital letter
+  let upperCaseLtters = /[A-Z]/g;
+  if (myInput.value.match(upperCaseLtters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
 
-
+  // validate length
+  if (myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+};
