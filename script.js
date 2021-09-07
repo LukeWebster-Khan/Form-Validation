a85aaa03d0408f4aa9176fa8bce0fd71bb5682da
Luke <lukewebsterkhan@gmail.com>
2021-09-07 22:36:13 +0100

    credit card validation with luhn algorithm added along with relevant calling validation divs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // onclick for password field, display message
 const myInput = document.getElementById("inputname");
 const eInput = document.getElementById("eInput");
+const eCard = document.getElementById("creditInput");
 const validEmail = document.getElementById("validEmail");
 const creditInput = document.getElementById("creditInput");
 const userName = document.getElementById("userName");
 const userEmail = document.getElementById("userEmail");
+const userCard = document.getElementById("userCard");
 
 myInput.onfocus = function () {
   document.getElementById("message").style.display = "block";
@@ -44,16 +46,41 @@ eInput.onblur = function () {
   document.getElementById("eMsg").style.display = "none";
 };
 
-// validate email /[A-Z]/g;       /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
-console.log(validEmail);
-// eInput.onfocus = function () {
-//   let email = /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
-//   if (eInput.value.match(email)) {
-//     console.log(validEmail);
-//     validEmail.classList.remove("invalidEmail");
-//     validEmail.classList.add("validEmail");
-//   } else {
-//     validEmail.classList.remove("validEmail");
-//     validEmail.classList.add("invalidEmail");
-//   }
-// };
+// Luhn algorithm & card validation
+function luhnCheck(val) {
+  var sum = 0;
+  for (var i = 0; i < val.length; i++) {
+    var intVal = parseInt(val.substr(i, 1));
+    if (i % 2 == 0) {
+      intVal *= 2;
+      if (intVal > 9) {
+        intVal = 1 + (intVal % 10);
+      }
+    }
+    sum += intVal;
+  }
+  return sum % 10 == 0;
+}
+
+creditInput.onkeyup = function validateCardNumber() {
+  var regex = new RegExp("^[0-9]{16}$");
+  let number = document.querySelector("#creditInput").value;
+
+  if (!regex.test(number)) {
+    userCard.classList.remove("validCard");
+    userCard.classList.add("invalidCard");
+    console.log("false");
+  } else {
+    userCard.classList.remove("invalidCard");
+    userCard.classList.add("validCard");
+    console.log("true");
+  }
+};
+
+eCard.onfocus = function () {
+  document.getElementById("eCard").style.display = "block";
+};
+
+eCard.onblur = function () {
+  document.getElementById("eCard").style.display = "none";
+};
