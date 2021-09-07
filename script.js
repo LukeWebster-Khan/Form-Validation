11cc854f67fe7a2b4a4e0a2b6fef5f47e25bf665
Luke <lukewebsterkhan@gmail.com>
2021-09-07 21:05:13 +0100

    fixed email validation

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-const capital = document.getElementById("capital");
-const length = document.getElementById("length");
 // onclick for password field, display message
 const myInput = document.getElementById("inputname");
-const Einput = document.getElementById("eInput");
+const eInput = document.getElementById("eInput");
 const validEmail = document.getElementById("validEmail");
 const creditInput = document.getElementById("creditInput");
-
-function isUserNameValid(username) {
-  /* 
-      Usernames can only have: 
-      - Lowercase Letters (a-z) 
-      - Numbers (0-9)
-      - Dots (.)
-      - Underscores (_)
-    */
-  const res = /^[a-z0-9_\.]+$/.exec(username);
-  const valid = !!res;
-  return valid;
-}
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
 
 myInput.onfocus = function () {
   document.getElementById("message").style.display = "block";
@@ -27,35 +14,25 @@ myInput.onblur = function () {
   document.getElementById("message").style.display = "none";
 };
 
-// When user types in field
 myInput.onkeyup = function () {
-  // validate lowercase letter
-  let lowerCaseLetters = /[a-z]/g;
-  if (myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
+  const validUser = /^([a-zA-Z0-9 _-]+)$/;
+  if (myInput.value.match(validUser)) {
+    userName.classList.remove("invalid");
+    userName.classList.add("valid");
   } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
+    userName.classList.remove("valid");
+    userName.classList.add("invalid");
   }
+};
 
-  // validate capital letter
-  let upperCaseLtters = /[A-Z]/g;
-  if (myInput.value.match(upperCaseLtters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
+eInput.onkeyup = function () {
+  const validUser = /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
+  if (eInput.value.match(validUser)) {
+    userEmail.classList.remove("invalidEmail");
+    userEmail.classList.add("validEmail");
   } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // validate length
-  if (myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
+    userEmail.classList.remove("validEmail");
+    userEmaile.classList.add("invalidEmail");
   }
 };
 
@@ -68,17 +45,15 @@ eInput.onblur = function () {
 };
 
 // validate email /[A-Z]/g;       /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
-
-Einput.onblur = function () {
-  let email = /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
-  if (eInput.value.match(email)) {
-    validEmail.classList.remove("invalid");
-    validEmail.classList.add("valid");
-    validEmail.innerHTML = "Entered email is valid";
-  } else {
-    validEmail.classList.remove("valid");
-    validEmail.classList.add("invalid");
-    validEmail.innerHTML =
-      "Please enter a valid email address such as: user@user.co.uk";
-  }
-};
+console.log(validEmail);
+// eInput.onfocus = function () {
+//   let email = /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
+//   if (eInput.value.match(email)) {
+//     console.log(validEmail);
+//     validEmail.classList.remove("invalidEmail");
+//     validEmail.classList.add("validEmail");
+//   } else {
+//     validEmail.classList.remove("validEmail");
+//     validEmail.classList.add("invalidEmail");
+//   }
+// };
